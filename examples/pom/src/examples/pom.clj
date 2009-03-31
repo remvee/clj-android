a6a07de509449d7dd96e14225f9f95307da7ecee
Remco van 't Veer <rwvtveer@xs4all.nl>
2009-03-31 11:16:26 +0200

    same message as BSD version

--- examples/pom/src/examples/pom.clj
+++ examples/pom/src/examples/pom.clj
@@ -1,5 +1,19 @@
 (ns examples.pom
-  (:use clj-android pom))
+  (:use clj-android pom)
+  (:import java.util.Calendar))
 
 (defactivity Main
-  (:resume (content-view [TextView {:text (str (Math/round (phase-of-the-moon)) "% of full")}])))
+  (:resume (let [today (phase-of-the-moon)
+                 tomorrow (phase-of-the-moon
+                           (doto (Calendar/getInstance) (.add Calendar/HOUR 24)))
+                 message (str
+                          "The Moon is "
+                          (cond (= 100 (Math/round today)) "Full"
+                                (= 0  (Math/round today)) "New"
+                                (= 50  (Math/round today))
+                                (if (> tomorrow today) "at the First Quarter" "at the Last Quarter")
+                                true (str
+                                      (if (> tomorrow today) "Waxing" "Waning") " "
+                                      (if (> today 50) "Gibbous" "Crescent ")
+                                      (format " (%1.0f%% of Full)" today))))]
+             (content-view [TextView {:text message}]))))
